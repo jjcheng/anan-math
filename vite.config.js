a22e918f52538526f7f891ddffee85814df8afae
jjcheng <chengjianjin@gmail.com>
2026-02-28 18:52:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/anan-math/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/anan-math/' : '/',
   plugins: [react()],
-})
+}))
